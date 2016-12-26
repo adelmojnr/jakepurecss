06aaebe55fa2feeb6bcaa41820c2f9a300d1a715
Adelmo Junior <junior-adelmo@hotmail.com>
2016-12-25 22:32:53 -0300

    subindooo os ajustessss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', () =>
-    gulp.src('css/jake.css')
+    gulp.src('css/style.css')
         .pipe(autoprefixer({
             browsers: ['last 7 versions'],
             cascade: false
